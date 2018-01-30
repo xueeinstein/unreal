c5f0e96d0cb52d67ad03c9f3426b50e03742bc3a
Bill Xue <xueeinstein@gmail.com>
2018-01-30 12:49:43 +0800

    reformat code style using yapf

--- test.py
+++ test.py
@@ -9,19 +9,24 @@ import train.rmsprop_applier_test
 import environment.environment_test
 import model.model_test
 
-def get_suite():
-  suite = unittest.TestSuite()
 
-  suite.addTest(unittest.makeSuite(train.experience_test.TestExperience))
-  suite.addTest(unittest.makeSuite(train.rmsprop_applier_test.TestRMSPropApplier))    
-  suite.addTest(unittest.makeSuite(environment.environment_test.TestEnvironment))
-  suite.addTest(unittest.makeSuite(model.model_test.TestUnrealModel))
-  
-  return suite
+def get_suite():
+    suite = unittest.TestSuite()
+
+    suite.addTest(unittest.makeSuite(train.experience_test.TestExperience))
+    suite.addTest(
+        unittest.makeSuite(train.rmsprop_applier_test.TestRMSPropApplier))
+    suite.addTest(
+        unittest.makeSuite(environment.environment_test.TestEnvironment))
+    suite.addTest(unittest.makeSuite(model.model_test.TestUnrealModel))
+
+    return suite
+
 
 def main():
-  suite = get_suite()
-  unittest.TextTestRunner().run(suite)
+    suite = get_suite()
+    unittest.TextTestRunner().run(suite)
+
 
 if __name__ == '__main__':
-  main()
+    main()
